38e73883ec3a1345dcb7c892c959be010fbe6888
migellito <migellitos@gmail.com>
2022-09-15 09:06:18 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,84 +1,111 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    //  Eating
+input.onButtonPressed(Button.A, function () {
+    // Eating
     basic.showLeds(`
         . # . # .
-                . . . . .
-                # . # . #
-                # . . . #
-                # # # # #
-    `)
+        . . . . .
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
     feeded = 1
     food_timer = 0
     poo_timer = 0
 })
-input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
+input.onGesture(Gesture.LogoUp, function () {
     basic.showIcon(IconNames.Happy)
+    food_timer = 0
+    mood = 19
 })
-input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+input.onGesture(Gesture.TiltLeft, function () {
     basic.showLeds(`
         . . . . .
-                . . . . #
-                . . . . #
-                # # # # #
-                # . . . #
-    `)
+        . . . . #
+        . . . . #
+        # # # # #
+        # . . . #
+        `)
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         . . . . .
-                # . . # #
-                # # # # .
-                . # . # .
-                # # . # .
-    `)
+        # . . # #
+        # # # # .
+        . # . # .
+        # # . # .
+        `)
+    poo_timer = 0
 })
-input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+input.onGesture(Gesture.TiltRight, function () {
     basic.showLeds(`
         . . . . .
-                . . . . #
-                . . . . #
-                # # # # #
-                # . . . #
-    `)
+        . . . . #
+        . . . . #
+        # # # # #
+        # . . . #
+        `)
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
 })
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+let mood = 0
 let poo_timer = 0
 let food_timer = 0
 let feeded = 0
 music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
 basic.showIcon(IconNames.Target)
-basic.forever(function on_forever() {
-    
-    basic.pause(1000)
-    food_timer += 1
-    if (feeded == 1) {
-        if (food_timer == 120) {
-            basic.showIcon(IconNames.Angry)
-            music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 988, 1, 247, 0, 4758, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-        }
-        
-    } else if (food_timer == 10) {
-        basic.showIcon(IconNames.Angry)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 988, 1, 247, 0, 4758, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-    }
-    
-})
-basic.forever(function on_forever2() {
-    
-    basic.pause(1000)
-    poo_timer += 1
-    if (poo_timer == 20) {
+basic.forever(function () {
+    if (poo_timer == 600) {
         basic.showLeds(`
             . . . . .
-                        . . . . .
-                        . . . # .
-                        . . # # #
-                        . # # # #
-        `)
+            . . . . .
+            . . . # .
+            . . # # #
+            . # # # #
+            `)
         music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 3468, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
     }
-    
+})
+basic.forever(function () {
+    if (feeded == 1) {
+        basic.pause(1000)
+        poo_timer += 1
+    }
+})
+basic.forever(function () {
+    basic.pause(1000)
+    food_timer += 1
+})
+basic.forever(function () {
+    if (feeded == 0) {
+        if (food_timer >= 600) {
+            mood = 7
+        }
+    } else {
+        if (food_timer >= 36000) {
+            mood = 7
+        }
+    }
+})
+basic.forever(function () {
+    if (mood == 7) {
+        basic.showIcon(IconNames.No)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+    }
 })
